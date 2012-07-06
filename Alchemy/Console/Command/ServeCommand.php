0c95b6bd22aa46eec927992b45301c619e6c29d8
Erik Amaru Ortiz <erik@colosa.com>
2012-07-06 19:33:14 -0400

    update

--- Alchemy/Console/Command/ServeCommand.php
+++ Alchemy/Console/Command/ServeCommand.php
@@ -3,7 +3,9 @@ namespace Alchemy\Console\Command;
 
 use Symfony\Component\Console\Input\InputArgument,
     Symfony\Component\Console\Input\InputOption,
-    Symfony\Component\Console\Command\Command;
+    Symfony\Component\Console\Command\Command,
+    Symfony\Component\Console\Input\InputInterface,
+    Symfony\Component\Console\Output\OutputInterface;
 
 use Alchemy\Config;
 
@@ -54,7 +56,7 @@ class ServeCommand extends Command
     /**
      * @see Console\Command\Command
      */
-    protected function execute(Console\Input\InputInterface $input, Console\Output\OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output)
     {
         $host = $input->getOption('host') ? $this->config->get('dev_appserver.host') : $input->getOption('host');
         $port = $input->getOption('port') ? $this->config->get('dev_appserver.port') : $input->getOption('port');
