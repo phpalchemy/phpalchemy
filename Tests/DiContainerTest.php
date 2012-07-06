9092cd6ea205f153e4defa233038fa052c676932
Erik Amaru Ortiz <aortiz.erik@gmail.com>
2012-07-06 17:13:19 -0400

    conding standard update

--- Tests/DiContainerTest.php
+++ Tests/DiContainerTest.php
@@ -4,39 +4,41 @@
  */
 include __DIR__ . '/../DiContainer.php';
 
-class Service{}
+class Service
+{
+}
 
 class DiContainerTest extends \PHPUnit_Framework_TestCase
 {
     public function testWithString()
     {
-        $pimple = new DiContainer();
-        $pimple['param'] = 'value';
+        $diContainer = new DiContainer();
+        $diContainer['param'] = 'value';
 
-        $this->assertEquals('value', $pimple['param']);
+        $this->assertEquals('value', $diContainer['param']);
     }
 
     public function testWithClosure()
     {
-        $pimple = new DiContainer();
-        $pimple['service'] = function () {
+        $diContainer = new DiContainer();
+        $diContainer['service'] = function () {
             return new Service();
         };
 
-        $this->assertInstanceOf('Service', $pimple['service']);
+        $this->assertInstanceOf('Service', $diContainer['service']);
     }
 
     public function testServicesShouldBeDifferent()
     {
-        $pimple = new DiContainer();
-        $pimple['service'] = function () {
+        $diContainer = new DiContainer();
+        $diContainer['service'] = function () {
             return new Service();
         };
 
-        $serviceOne = $pimple['service'];
+        $serviceOne = $diContainer['service'];
         $this->assertInstanceOf('Service', $serviceOne);
 
-        $serviceTwo = $pimple['service'];
+        $serviceTwo = $diContainer['service'];
         $this->assertInstanceOf('Service', $serviceTwo);
 
         $this->assertNotSame($serviceOne, $serviceTwo);
@@ -44,40 +46,40 @@ class DiContainerTest extends \PHPUnit_Framework_TestCase
 
     public function testShouldPassContainerAsParameter()
     {
-        $pimple = new DiContainer();
-        $pimple['service'] = function () {
+        $diContainer = new DiContainer();
+        $diContainer['service'] = function () {
             return new Service();
         };
-        $pimple['container'] = function ($container) {
+        $diContainer['container'] = function ($container) {
             return $container;
         };
 
-        $this->assertNotSame($pimple, $pimple['service']);
-        $this->assertSame($pimple, $pimple['container']);
+        $this->assertNotSame($diContainer, $diContainer['service']);
+        $this->assertSame($diContainer, $diContainer['container']);
     }
 
     public function testIsset()
     {
-        $pimple = new DiContainer();
-        $pimple['param'] = 'value';
-        $pimple['service'] = function () {
+        $diContainer = new DiContainer();
+        $diContainer['param'] = 'value';
+        $diContainer['service'] = function () {
             return new Service();
         };
 
-        $pimple['null'] = null;
+        $diContainer['null'] = null;
 
-        $this->assertTrue(isset($pimple['param']));
-        $this->assertTrue(isset($pimple['service']));
-        $this->assertTrue(isset($pimple['null']));
-        $this->assertFalse(isset($pimple['non_existent']));
+        $this->assertTrue(isset($diContainer['param']));
+        $this->assertTrue(isset($diContainer['service']));
+        $this->assertTrue(isset($diContainer['null']));
+        $this->assertFalse(isset($diContainer['non_existent']));
     }
 
     public function testConstructorInjection ()
     {
         $params = array("param" => "value");
-        $pimple = new DiContainer($params);
+        $diContainer = new DiContainer($params);
 
-        $this->assertSame($params['param'], $pimple['param']);
+        $this->assertSame($params['param'], $diContainer['param']);
     }
 
     /**
@@ -86,41 +88,41 @@ class DiContainerTest extends \PHPUnit_Framework_TestCase
      */
     public function testOffsetGetValidatesKeyIsPresent()
     {
-        $pimple = new DiContainer();
-        echo $pimple['foo'];
+        $diContainer = new DiContainer();
+        echo $diContainer['foo'];
     }
 
     public function testOffsetGetHonorsNullValues()
     {
-        $pimple = new DiContainer();
-        $pimple['foo'] = null;
-        $this->assertNull($pimple['foo']);
+        $diContainer = new DiContainer();
+        $diContainer['foo'] = null;
+        $this->assertNull($diContainer['foo']);
     }
 
     public function testUnset()
     {
-        $pimple = new DiContainer();
-        $pimple['param'] = 'value';
-        $pimple['service'] = function () {
+        $diContainer = new DiContainer();
+        $diContainer['param'] = 'value';
+        $diContainer['service'] = function () {
             return new Service();
         };
 
-        unset($pimple['param'], $pimple['service']);
-        $this->assertFalse(isset($pimple['param']));
-        $this->assertFalse(isset($pimple['service']));
+        unset($diContainer['param'], $diContainer['service']);
+        $this->assertFalse(isset($diContainer['param']));
+        $this->assertFalse(isset($diContainer['service']));
     }
 
     public function testShare()
     {
-        $pimple = new DiContainer();
-        $pimple['shared_service'] = $pimple->share(function () {
+        $diContainer = new DiContainer();
+        $diContainer['shared_service'] = $diContainer->share(function () {
             return new Service();
         });
 
-        $serviceOne = $pimple['shared_service'];
+        $serviceOne = $diContainer['shared_service'];
         $this->assertInstanceOf('Service', $serviceOne);
 
-        $serviceTwo = $pimple['shared_service'];
+        $serviceTwo = $diContainer['shared_service'];
         $this->assertInstanceOf('Service', $serviceTwo);
 
         $this->assertSame($serviceOne, $serviceTwo);
@@ -128,32 +130,36 @@ class DiContainerTest extends \PHPUnit_Framework_TestCase
 
     public function testProtect()
     {
-        $pimple = new DiContainer();
-        $callback = function () { return 'foo'; };
-        $pimple['protected'] = $pimple->protect($callback);
+        $diContainer = new DiContainer();
+        $callback = function () {
+            return 'foo';
+        };
+        $diContainer['protected'] = $diContainer->protect($callback);
 
-        $this->assertSame($callback, $pimple['protected']);
+        $this->assertSame($callback, $diContainer['protected']);
     }
 
     public function testGlobalFunctionNameAsParameterValue()
     {
-        $pimple = new DiContainer();
-        $pimple['global_function'] = 'strlen';
-        $this->assertSame('strlen', $pimple['global_function']);
+        $diContainer = new DiContainer();
+        $diContainer['global_function'] = 'strlen';
+        $this->assertSame('strlen', $diContainer['global_function']);
     }
 
     public function testRaw()
     {
-        $pimple = new DiContainer();
-        $pimple['service'] = $definition = function () { return 'foo'; };
-        $this->assertSame($definition, $pimple->raw('service'));
+        $diContainer = new DiContainer();
+        $diContainer['service'] = $definition = function () {
+            return 'foo';
+        };
+        $this->assertSame($definition, $diContainer->raw('service'));
     }
 
     public function testRawHonorsNullValues()
     {
-        $pimple = new DiContainer();
-        $pimple['foo'] = null;
-        $this->assertNull($pimple->raw('foo'));
+        $diContainer = new DiContainer();
+        $diContainer['foo'] = null;
+        $this->assertNull($diContainer->raw('foo'));
     }
 
     /**
@@ -162,29 +168,29 @@ class DiContainerTest extends \PHPUnit_Framework_TestCase
      */
     public function testRawValidatesKeyIsPresent()
     {
-        $pimple = new DiContainer();
-        $pimple->raw('foo');
+        $diContainer = new DiContainer();
+        $diContainer->raw('foo');
     }
 
     public function testExtend()
     {
-        $pimple = new DiContainer();
-        $pimple['shared_service'] = $pimple->share(function () {
+        $diContainer = new DiContainer();
+        $diContainer['shared_service'] = $diContainer->share(function () {
             return new Service();
         });
 
         $value = 12345;
 
-        $pimple->extend('shared_service', function($sharedService) use ($value) {
+        $diContainer->extend('shared_service', function($sharedService) use ($value) {
             $sharedService->value = $value;
             return $sharedService;
         });
 
-        $serviceOne = $pimple['shared_service'];
+        $serviceOne = $diContainer['shared_service'];
         $this->assertInstanceOf('Service', $serviceOne);
         $this->assertEquals($value, $serviceOne->value);
 
-        $serviceTwo = $pimple['shared_service'];
+        $serviceTwo = $diContainer['shared_service'];
         $this->assertInstanceOf('Service', $serviceTwo);
         $this->assertEquals($value, $serviceTwo->value);
 
@@ -197,8 +203,9 @@ class DiContainerTest extends \PHPUnit_Framework_TestCase
      */
     public function testExtendValidatesKeyIsPresent()
     {
-        $pimple = new DiContainer();
-        $pimple->extend('foo', function () {});
+        $diContainer = new DiContainer();
+        $diContainer->extend('foo', function () {
+        });
     }
 
     /**
@@ -207,17 +214,19 @@ class DiContainerTest extends \PHPUnit_Framework_TestCase
      */
     public function testExtendValidatesKeyYieldsObjectDefinition()
     {
-        $pimple = new DiContainer();
-        $pimple['foo'] = 123;
-        $pimple->extend('foo', function () {});
+        $diContainer = new DiContainer();
+        $diContainer['foo'] = 123;
+        $diContainer->extend('foo', function () {
+        });
     }
 
     public function testKeys()
     {
-        $pimple = new DiContainer();
-        $pimple['foo'] = 123;
-        $pimple['bar'] = 123;
+        $diContainer = new DiContainer();
+        $diContainer['foo'] = 123;
+        $diContainer['bar'] = 123;
 
-        $this->assertEquals(array('foo', 'bar'), $pimple->keys());
+        $this->assertEquals(array('foo', 'bar'), $diContainer->keys());
     }
 }
+
