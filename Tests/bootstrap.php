ce6a92bd794adf19c724666cf29bc96f44fad668
Erik Amaru Ortiz <aortiz.erik@gmail.com>
2012-08-24 23:01:38 -0400

    updating components

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,12 +1,13 @@
 <?php
 // Tests/bootstrap.php
 
-// $basePath = realpath(__DIR__ . '/../') . DIRECTORY_SEPARATOR;
+$basePath = realpath(__DIR__ . '/../') . DIRECTORY_SEPARATOR;
 
-// $loader = include $basePath . 'Tests/tools/autoload.php';
-// $loader->add('Alchemy', $basePath);
+$loader = include $basePath . 'Tests/tools/autoload.php';
+$loader->add('Alchemy', $basePath);
+require_once __DIR__ . '/../vendor/autoload.php';
 
 //$loader->add('Symfony', $basePath . 'vendor/symfony/http-foundation/');
 
-require 'autoload.php';
+//require 'autoload.php';
 
