c0d13589f2e27f9247bf4defdbb2486b5df75acf
@Sindreik <aortiz.erik@icloud.com>
2013-07-02 21:33:27 -0400

    removing unused code and updating references

--- autoload.php
+++ autoload.php
@@ -21,24 +21,9 @@ $classLoader = Alchemy\Component\ClassLoader\ClassLoader::getInstance();
 $classLoader->register('Alchemy', __DIR__ . DIRECTORY_SEPARATOR);
 $classLoader->register('Notoj', __DIR__ . '/vendor/crodas/Notoj/lib/');
 $classLoader->register('Symfony', __DIR__ . '/vendor/symfony/console/');
-$classLoader->register('Zend\Filter\\', __DIR__ . '/vendor/zendframework/zend-filter/');
+//$classLoader->register('Zend\Filter\\', __DIR__ . '/vendor/zendframework/zend-filter/');
 
 set_exception_handler(array(new Alchemy\Exception\Handler(), 'handle'));
 
-/*if (! file_exists(__DIR__ . '/vendor/autoload.php')) {
-    throw new Exception(sprintf(
-        "Vendors for phpalchemy are missing.\nPlease execute those commands on phpalchemy home dir.:\n" .
-        "\$>cd /path/to/phpalchemy\n\$>curl -s http://getcomposer.org/installer | php\n\$>php composer.phar install"
-    ));
-}*/
-
-//TODO this should't be loading evertime, just when it is used.
+//TODO this should't be loading everytime, just when it is used.
 require_once 'crodas/haanga/lib/Haanga.php';
-
-/*set_include_path(
-    __DIR__ . DIRECTORY_SEPARATOR . 'vendor'  . PATH_SEPARATOR .
-    __DIR__ . '/vendor/pear-pear.php.net'      .  PATH_SEPARATOR .
-    __DIR__ . '/vendor/pear-pear.php.net/PEAR' .  PATH_SEPARATOR .
-    get_include_path()
-);*/
-
