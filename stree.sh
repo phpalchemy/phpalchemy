6311fdfa28f3fa75572f47e1c68465210be4c00e
@Sindreik <aortiz.erik@icloud.com>
2013-07-04 00:33:22 -0400

    updating references

--- stree.sh
+++ stree.sh
@@ -1,8 +1,8 @@
 repo=$1
-git remote add -f $repo git@bitbucket.org:eriknyk/$repo.git
+git remote add -f $repo git@github.com:phpalchemy/$repo.git
 git merge -s ours --no-commit $repo/master
 git read-tree --prefix=Alchemy/Component/$repo/ -u $repo/master
-git commit -m "Merge branch 'master' of bitbucket.org:eriknyk/$repo"
+git commit -m "Merge branch 'master' of github.com:phpalchemy/$repo"
 
 # this is to update the subtrees
 #git pull -s subtree $repo master
